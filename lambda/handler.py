0c9be104055f0009fe7cfdf33b203c1ed21c16e6
bharath150296 <bharathg1596@gmail.com>
2025-12-16 11:40:23 +0530

    adding post api

--- lambda/handler.py
+++ lambda/handler.py
@@ -13,14 +13,32 @@ sns = boto3.client('sns')
 table = dynamodb.Table(TABLE_NAME)
 
 def lambda_handler(event, context):
-    user_id = str(uuid.uuid4())
-    message = event.get('message', 'Hello, World!')
+
+    http_method = event.get('httpMethod')
+    path = event.get('path')
+    # if http_method == 'POST' and path == '/app':
+    body = json.loads(event.get('body', '{}'))
+    phone_number = body.get('phoneNumber')
+    message = body.get('message', 'Default message from POST')
+    if not phone_number:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'error': 'phoneNumber is required'})
+        }
     table.put_item(
         Item={
-            'UserId': user_id,
+            'id': phone_number,
             'Message': message
         }
     )
+    user_id = str(uuid.uuid4())
+    # message = event.get('message', 'Hello, World!')
+    # table.put_item(
+    #     Item={
+    #         'id': user_id,
+    #         'Message': message
+    #     }
+    # )
     sns.publish(
         TopicArn=SNS_TOPIC_ARN,
         Message='Lambda executed successfully'
